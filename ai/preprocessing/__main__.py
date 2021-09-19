0d564b863cb15f632d09e28d38e11904dcabb7ed
Bill Cui <billcui2001@hotmail.com>
2021-09-19 08:38:13 -0400

    working on api

--- ai/preprocessing/__main__.py
+++ ai/preprocessing/__main__.py
@@ -1,7 +1,7 @@
 from dotenv import load_dotenv
 from pymongo import MongoClient
 from get_data import insert_raw_data
-from add_features import add_features
+from add_features import fill_preprocessed
 import sys
 import os
 
@@ -10,14 +10,12 @@ load_dotenv()
 
 def main():
 
-    print(os.environ.get("CLIENT_ACCESS_TOKEN"))
-
     client = MongoClient('mongodb://localhost:27017/')
 
     if(len(sys.argv) > 1 and sys.argv[1] == "--reset"):
         insert_raw_data(client=client)
 
-    add_features(client=client)
+    fill_preprocessed(client=client)
 
 
 if __name__ == "__main__":
--- server/app/routes.py
+++ server/app/routes.py
@@ -6,7 +6,7 @@ from datetime import datetime
 from flask import jsonify
 from . import app
 from os.path import abspath
-
+from .add_features import get_added_features
 from joblib import dump, load
 import os
 dirname = os.path.dirname(__file__)
@@ -15,96 +15,16 @@ clf = load(os.path.join(dirname, './model.joblib'))
 
 
 
-import re
-import functools
-import tensorflow as tf
-import nltk
-from typing import List, Set, Dict, Tuple, Optional
-from functools import reduce
-from collections import Counter
-from statistics import mean
-import os
-import numpy as np
-
-
-def split_into_lines(lyrics: str) -> List[str]:
-    lines: List[str] = lyrics.splitlines()
-
-    result: List[str] = []
-
-    for line in lines:
-        if line != "":
-            result.append(str(line))
-
-    return result
-
-
-def filter_out_non_important(lyrics: str):
-
-    tokens = nltk.word_tokenize(lyrics)
-
-    filtered_tokens = []
-
-    for token in nltk.pos_tag(tokens):
-
-        if(re.match("[a-z|A-Z|0-9]*'", token[0])):
-            continue
-
-        if(re.match("[a-z|A-Z|0-9]*[eE]mbed", token[0])):
-            continue
-
-        if(token[0] == "[" or token[0] == "]"):
-            continue
-
-        if (token[1] == "NN" or
-            token[1] == "RB" or
-            token[1] == "VB"
-            ):
-            filtered_tokens.append(token[0])
-
-    return " ".join(str(x) for x in filtered_tokens)
-
-
-# excludes non {noun, verb, adjective}
-def get_repetition_score(lyrics: str):
-    
-    lines = split_into_lines(str(lyrics))
-
-    for index,line in enumerate(lines):
-        lines[index] =  filter_out_non_important(line)
-    
-
-    if len(lines) == 0:
-        return None
-
-    tokenizer = tf.keras.preprocessing.text.Tokenizer(
-        num_words=None,
-    )
-
-    tokenizer.fit_on_texts(lines)
-
-    tfidf_matrix = tokenizer.texts_to_matrix(lines, mode='tfidf')
-
-    return np.average(tfidf_matrix)
-
-
-def get_features(lyrics):
-    repetition_score = get_repetition_score(lyrics)
-
-    if repetition_score == None:
-        return None
-
-    
-    return np.array([[repetition_score]])
-
 
 @app.route("/", methods=['POST'])
 def index():
     lyrics = request.json
 
 
-    parameter = get_features(lyrics)
+    parameters_dict:dict = get_added_features(lyrics)
 
-    print(clf.predict_proba(parameter))
+    print(parameters_dict.values())
 
-    return jsonify({"result": clf.predict_proba(parameter)[0][1]})
+    print(clf.predict_proba(list(parameters_dict.values())))
+
+    return jsonify({"result": clf.predict_proba(clf.predict_proba(list(parameters_dict.values())))[0][1]})
